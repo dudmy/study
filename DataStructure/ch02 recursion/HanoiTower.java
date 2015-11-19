aea5224440e6fceb08383d7cb3144a9fe11ee46a
dudmy <dudmy@dudmy.net>
2015-11-19 14:09:04 +0900

    자료구조 폴더 생성 및 하노이탑 코드 작성

---
+++ DataStructure/ch02 recursion/HanoiTower.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+/*
+ * 2.8 하노이탑 문제 프로그램
+ */
+public class HanoiTower {
+
+	void hanoi_tower(int n, char from, char tmp, char to) {
+		if (n == 1) {
+			System.out.println("원판 1을 " + from + "에서 " + to + "으로 옮긴다.");
+		} else {
+			hanoi_tower(n-1, from, to, tmp);
+			System.out.println("원판 " + n + "을 " + from + "에서 " + to + "으로 옮긴다.");
+			hanoi_tower(n-1, tmp, from, to);
+		}
+	}
+	
+	public static void main(String[] args) {
+		HanoiTower ht = new HanoiTower();
+		ht.hanoi_tower(3, 'A', 'B', 'C');
+	}
+}
